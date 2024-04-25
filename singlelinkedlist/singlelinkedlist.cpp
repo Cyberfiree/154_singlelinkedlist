93459d5d8e89ce3e68912c358e7b61d06781da1f
Cyberfire <buayagoreng99@gmail.com>
2024-04-25 12:03:34 +0800

    Fungsi untuk mencari dan menampilkan data mahasiswa berdasarkan NIM

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -118,6 +118,30 @@ void traverse() {
     }
 }
 
+// Fungsi untuk mencari dan menampilkan data mahasiswa berdasarkan NIM
+void searchData() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else {
+        int nim;
+        cout << "Masukkan NIM: ";
+        cin >> nim;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            if (currentNode->noMhs == nim) {
+                cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+                return;
+            }
+            currentNode = currentNode->next;
+        }
+        cout << "Data tidak ditemukan" << endl;
+    }
+}
+
 int main()
 {
 }
