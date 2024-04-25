0283489831006bde16ffe409bc5046fd7214512d
Cyberfire <buayagoreng99@gmail.com>
2024-04-25 11:47:08 +0800

    Mendefinisikan struktur Node untuk representasi data mahasiswa

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+using namespace std;
+
+// Mendefinisikan struktur Node untuk representasi data mahasiswa
+struct Node {
+    int noMhs; //Nomor Mahasiswa
+    string name; // Nama Mahasiswa
+    Node* next; // pointer ke node berikutnya
+};
+
 int main()
 {
 }
